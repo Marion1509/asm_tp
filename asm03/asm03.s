29aa62eefb3b752a5873ea7c4ac7459ca3c40f2e
Marion Delthil <145343805+Marion1509@users.noreply.github.com>
2024-04-16 09:07:03 +0200

    Add files via upload

---
+++ asm03/asm03.s
@@ -0,0 +1,34 @@
+global _start
+
+section .data
+    msg db "1337", 10 
+
+section .text
+
+_start:
+    
+    mov rsi, [rsp + 8]     
+    mov rsi, [rsi]         
+
+    
+    cmp rsi, 42
+    je is_42               
+
+not_42:
+    
+    mov rax, 60            
+    xor rdi, rdi           
+    syscall                
+
+is_42:
+    
+    mov rax, 1             
+    mov rdi, 1             
+    mov rdx, 5             
+    mov rsi, msg           
+    syscall                
+
+   
+    xor rax, rax           
+    xor rdi, rdi           
+    syscall                
