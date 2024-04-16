17a172f616b845101cf222265a817f64af880f90
Marion Delthil <145343805+Marion1509@users.noreply.github.com>
2024-04-16 09:24:37 +0200

    Add files via upload

---
+++ asm04/asm04.s
@@ -0,0 +1,22 @@
+global _start
+
+section .text
+
+_start:
+    
+    mov eax, [rsp + 8 ]
+    test eax, 1
+    jz _return_0
+
+    
+    mov eax, 1
+    jmp _exit
+
+_return_0:
+    
+    xor eax, eax
+
+_exit:
+    mov ebx, eax
+    mov eax, 60
+    syscall
