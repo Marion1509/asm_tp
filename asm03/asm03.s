630535893b153c2a26cdd665343ed2aaef89125d
Marion Delthil <145343805+Marion1509@users.noreply.github.com>
2024-06-10 15:25:12 +0200

    Update asm03.s

--- asm03/asm03.s
+++ asm03/asm03.s
@@ -6,30 +6,28 @@ section .data
 section .text
 
 _start:
-
     mov rsi, [rsp + 8]     
     mov rsi, [rsi]         
 
     
-    ; Comparer avec 42
     cmp rsi, 42
     je is_42               
 
 not_42:
-    ; Retourner 1
-    mov rax, 60           
+    
+    mov rax, 60            
     xor rdi, rdi           
     syscall                
 
 is_42:
-    ; Afficher "1337"
-    mov rax, 1            
+    
+    mov rax, 1             
     mov rdi, 1             
     mov rdx, 5             
     mov rsi, msg           
     syscall                
 
-    ; Retourner 0
+   
     xor rax, rax           
     xor rdi, rdi           
-    syscall                
+    syscall
