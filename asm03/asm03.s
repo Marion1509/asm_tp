06864922e0c76a7b5e74bfeed8f3fb79dc760c58
Marion Delthil <145343805+Marion1509@users.noreply.github.com>
2024-06-10 15:22:22 +0200

    Update asm03.s

--- asm03/asm03.s
+++ asm03/asm03.s
@@ -6,29 +6,30 @@ section .data
 section .text
 
 _start:
-    
+    ; Récupérer l'argument (le nombre)
     mov rsi, [rsp + 8]     
     mov rsi, [rsi]         
 
     
+    ; Comparer avec 42
     cmp rsi, 42
-    je is_42               
+    je is_42               ; Si égal à 42, sauter à is_42
 
 not_42:
-    
-    mov rax, 60            
-    xor rdi, rdi           
-    syscall                
+    ; Retourner 1
+    mov rax, 60            ; Code de sortie 1
+    xor rdi, rdi           ; nettoi rdi
+    syscall                ; Appel système pour terminer le programme
 
 is_42:
-    
-    mov rax, 1             
-    mov rdi, 1             
-    mov rdx, 5             
-    mov rsi, msg           
-    syscall                
+    ; Afficher "1337"
+    mov rax, 1             ; Appel système pour écrire sur la sortie standard
+    mov rdi, 1             ; Descripteur de fichier STDOUT
+    mov rdx, 5             ; Longueur du message (4 caractères + 1 pour le saut de ligne)
+    mov rsi, msg           ; Adresse du message
+    syscall                ; Appel système pour afficher le message
 
-   
-    xor rax, rax           
-    xor rdi, rdi           
-    syscall                
+    ; Retourner 0
+    xor rax, rax           ; Code de sortie 0
+    xor rdi, rdi           ; Clear rdi
+    syscall                ; Appel système pour terminer le programme
