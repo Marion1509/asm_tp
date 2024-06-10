6793a957985528e4dbd17efe2d7141d658832744
Marion Delthil <145343805+Marion1509@users.noreply.github.com>
2024-06-10 15:59:33 +0200

    Update asm05.s

--- asm05/asm05.s
+++ asm05/asm05.s
@@ -1,24 +1,69 @@
 global _start
 
-section .bss
+section.bss
     result_msg resb 50
 
-section .text
-
+section.text
 _start:
+    cmp edi, 2
+    jl no_args
+
     mov ebp, esp
+    mov eax, [ebp + 16]
+    mov ebx, [ebp + 24]
 
-    mov eax, [ebp + 8]  
-    mov ebx, [ebp + 12] 
-    
-    add eax, ebx
+    call atoi
+    mov esi, eax
+    mov eax, [ebp + 24]
+    call atoi
 
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, result_msg
-    mov edx, 30 
-    syscall
+    add eax, esi
+
+    mov esi, result_msg
+    call itoa
 
     mov eax, 1
-    xor ebx, ebx
+    mov edi, 1
+    mov esi, result_msg
+    mov edx, 50
     syscall
+
+    mov eax, 60
+    xor edi, edi
+    syscall
+
+no_args:
+    mov eax, 60
+    mov edi, 1
+    syscall
+
+atoi:
+    xor eax, eax
+    xor ecx, ecx
+loop_start:
+    movzx edx, byte [ebx + ecx]
+    cmp edx, '0'
+    jl end_loop
+    cmp edx, '9'
+    jg end_loop
+    sub edx, '0'
+    imul eax, 10
+    add eax, edx
+    inc ecx
+    jmp loop_start
+end_loop:
+    ret
+
+itoa:
+    xor ecx, ecx
+    mov ebx, 10
+loop_start:
+    xor edx, edx
+    div ebx
+    add dl, '0'
+    mov [esi + ecx], dl
+    inc ecx
+    cmp eax, 0
+    jne loop_start
+    mov byte [esi + ecx], 10
+    ret
