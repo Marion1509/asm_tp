cc8a273c983cab012bd212c84c823a2ee514458e
Marion Delthil <145343805+Marion1509@users.noreply.github.com>
2024-06-10 15:26:35 +0200

    Update asm03.s

--- asm03/asm03.s
+++ asm03/asm03.s
@@ -9,25 +9,21 @@ _start:
     mov rsi, [rsp + 8]     
     mov rsi, [rsi]         
 
-    
     cmp rsi, 42
-    je is_42               
-
-not_42:
-    
-    mov rax, 60            
-    xor rdi, rdi           
-    syscall                
+    jne not_42             
 
 is_42:
-    
     mov rax, 1             
     mov rdi, 1             
     mov rdx, 5             
     mov rsi, msg           
     syscall                
 
-   
     xor rax, rax           
     xor rdi, rdi           
-    syscall
+    syscall                 
+
+not_42:
+    mov rax, 60            
+    xor rdi, rdi           
+    syscall                
